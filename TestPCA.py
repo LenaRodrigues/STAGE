beab8da5f1a20fa7e3d7f31245f3569ef44f857c
LenaRodrigues <107058185+LenaRodrigues@users.noreply.github.com>
2022-06-22 15:26:58 +0200

    Update TestPCA.py

--- TestPCA.py
+++ TestPCA.py
@@ -151,7 +151,6 @@ RPPA         = RPPA.drop_duplicates(subset=['Hybridization REF'])
 tmp_list = np.asarray(list(RPPA))
 RPPA     = RPPA[tmp_list[RPPA.isna().sum(axis=0) == 0]]
 label = pd.read_csv('/home/lrodrigues/STAGE/DATAclinic/finalclinic.csv')
-print("4:", label.columns.tolist())
 label = label.sort_values(by='Composite.Element.REF').reset_index(drop=True)
 label = label[label['Composite.Element.REF'].apply(lambda x: 'tcga' in x)].drop_duplicates(subset=['Composite.Element.REF'], keep ='last').reset_index(drop=True)
 
