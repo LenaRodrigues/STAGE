4a33113729dc34a9453a83bc72a3f7ac97d8b70b
LenaRodrigues <107058185+LenaRodrigues@users.noreply.github.com>
2022-06-24 12:02:28 +0200

    Update data_process_TCGA_part2.py

--- data_process_TCGA_part2.py
+++ data_process_TCGA_part2.py
@@ -7,7 +7,6 @@ from sklearn.decomposition import PCA, SparsePCA, KernelPCA
 mRNAseq     = pd.read_csv('/home/lrodrigues/STAGE/DATAmRseq/finalmRNA.csv', low_memory=False)
 mRNAseq     = mRNAseq.rename(columns={'gene':'HYBRIDIZATION R'})
 mRNAseq     = mRNAseq.drop_duplicates(subset=['HYBRIDIZATION R']).reset_index(drop=True)
-mRNAseq     = mRNAseq[mRNAseq['HYBRIDIZATION REF'] != 'HYBRIDIZATION R'].reset_index(drop=True)
 mRNAseq     = mRNAseq.rename(columns={'HYBRIDIZATION REF':'Hybridization REF'})
 mRNAseq['Hybridization REF'] = mRNAseq['Hybridization REF'].apply(lambda x: x.lower()[:-3])
 
