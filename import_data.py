0c87699c8a67b8e6bf00f138ec9219db9d27d5eb
LenaRodrigues <107058185+LenaRodrigues@users.noreply.github.com>
2022-06-28 16:26:50 +0200

    def changes

--- import_data.py
+++ import_data.py
@@ -24,42 +24,37 @@ def import_incomplete_handwritten():
 
 
 def import_dataset_TCGA(year=1):
-    filepath = '/home/lrodrigues/STAGE/'.format(int(year))
-    data = ['Methylation','miRNA', 'mRNA', 'RPPA']
-    for m in data:
-        filename = '{}.npy'.format(int(year))
-        npz = np.load(filepath + filename )
-        Mask = npz['m']
-        M = np.shape(Mask)[1]
+    filename = '/home/lrodrigues/STAGE/multi_omics_{}yr_mortality.npz'.format(int(year))
+    npz = np.load(filename)
+    print(npz.files)
+    Y = npz['label']
+    Mask = np.zeros((Y.shape[0], 4))
+    i = 0
+    for m in ['Methylation', 'miRNAseq', 'mRNAseq', 'RPPA']:
+        vec = np.any(np.isnan(npz[m]), axis=1)
+        Mask[:, i] = vec
+        i = i+1
+
     X_set = {}
-    
-    for m in range(M):
-        tmp = npz['x{}'.format(m+1)]
+    for m in ['Methylation', 'miRNAseq', 'mRNAseq', 'RPPA']:
+        tmp = npz['Methylation'.format(1)]
         tmp[np.isnan(tmp[:, 0]), :] = np.nanmean(tmp, axis=0)
         X_set[m] = tmp
-
-    Y     = npz['y']
-
     X_set_incomp = {}
-    X_set_comp   = {}
+    X_set_comp = {}
     for m in range(M):
-        X_set_comp[m]    = X_set[m][np.sum(Mask, axis=1) == 4]
-        X_set_incomp[m]  = X_set[m][np.sum(Mask, axis=1) != 4]
-
-    Y_comp    = Y[np.sum(Mask, axis=1) == 4]
-    Y_incomp  = Y[np.sum(Mask, axis=1) != 4]
-
-    Mask_comp    = Mask[np.sum(Mask, axis=1) == 4]
-    Mask_incomp  = Mask[np.sum(Mask, axis=1) != 4]
-
-
+        X_set_comp[m] = X_set[m][np.sum(Mask, axis=1) == 4]
+        X_set_incomp[m] = X_set[m][np.sum(Mask, axis=1) != 4]
+    Y_comp = Y[np.sum(Mask, axis=1) == 4]
+    Y_incomp = Y[np.sum(Mask, axis=1) != 4]
+    Mask_comp = Mask[np.sum(Mask, axis=1) == 4]
+    Mask_incomp = Mask[np.sum(Mask, axis=1) != 4]
     Y_onehot_incomp = np.zeros([np.shape(Y_incomp)[0], 2])
-    Y_onehot_comp   = np.zeros([np.shape(Y_comp)[0], 2])
+    Y_onehot_comp = np.zeros([np.shape(Y_comp)[0], 2])
 
     Y_onehot_incomp[Y_incomp == 0, 0] = 1
     Y_onehot_incomp[Y_incomp == 1, 1] = 1
-
     Y_onehot_comp[Y_comp == 0, 0] = 1
     Y_onehot_comp[Y_comp == 1, 1] = 1
-    
+
     return X_set_comp, Y_onehot_comp, Mask_comp, X_set_incomp, Y_onehot_incomp, Mask_incomp
