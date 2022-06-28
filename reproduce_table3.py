957255107654eadc90c22cf838b46944a1cfb668
LenaRodrigues <107058185+LenaRodrigues@users.noreply.github.com>
2022-06-28 17:21:32 +0200

    Update reproduce_table3.py

--- reproduce_table3.py
+++ reproduce_table3.py
@@ -136,7 +136,8 @@ stop_flag = 0
 for itr in range(ITERATION):
     x_mb_set, y_mb, m_mb = f_get_minibatch_set(
         mb_size, tr_X_set, tr_Y_onehot, tr_M)
-
+    print(x_mb_set)
+    print(len(x_mb_set))
     _, Lt, Lp, Lkl, Lps, Lkls, Lc = model.train(
         x_mb_set, y_mb, m_mb, alpha, beta, lr_rate, k_prob)
 
