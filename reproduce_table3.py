8f36b0c28ae9082e4670ff2c9b272fc807dc0020
LenaRodrigues <107058185+LenaRodrigues@users.noreply.github.com>
2022-06-29 10:58:57 +0200

    Update reproduce_table3.py

--- reproduce_table3.py
+++ reproduce_table3.py
@@ -172,8 +172,10 @@ for itr in range(ITERATION):
     va_avg_Lkls += Lkls/STEPSIZE
     va_avg_Lc += Lc/STEPSIZE
     
-    print(va_X_set.keys())
-    print(va_M.keys())
+    va_X_set[0]=va_X_set.pop('Methylation')
+    va_X_set[1]=va_X_set.pop('miRNAseq')
+    va_X_set[2]=va_X_set.pop('mRNAseq')
+    va_X_set[3]=va_X_set.pop('RPPA')
     if (itr+1) % STEPSIZE == 0:
         y_pred, y_preds = model.predict_ys(va_X_set, va_M)
 
