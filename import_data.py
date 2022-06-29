3cb23d84bf7b78e8f61dc236bd1d5a66e6b00eb2
LenaRodrigues <107058185+LenaRodrigues@users.noreply.github.com>
2022-06-29 10:15:49 +0200

    Update import_data.py

--- import_data.py
+++ import_data.py
@@ -23,6 +23,7 @@ def import_incomplete_handwritten():
 
 
 
+
 def import_dataset_TCGA(year=1):
     filename = '/home/lrodrigues/STAGE/multi_omics_{}yr_mortality.npz'.format(int(year))
     npz = np.load(filename)
@@ -30,19 +31,19 @@ def import_dataset_TCGA(year=1):
     Y = npz['label']
     Mask = np.zeros((Y.shape[0], 4))
     i = 0
-    for m in range(0, 4):
+    for m in ['Methylation', 'miRNAseq', 'mRNAseq', 'RPPA']:
         vec = np.any(np.isnan(npz[m]), axis=1)
         Mask[:, i] = vec
         i = i+1
 
     X_set = {}
-    for m in range(0,4):
-        tmp = npz['m'.format(1)]
+    for m in ['Methylation', 'miRNAseq', 'mRNAseq', 'RPPA']:
+        tmp = npz['Methylation'.format(1)]
         tmp[np.isnan(tmp[:, 0]), :] = np.nanmean(tmp, axis=0)
         X_set[m] = tmp
     X_set_incomp = {}
     X_set_comp = {}
-    for m in range(0,4):
+    for m in ['Methylation', 'miRNAseq', 'mRNAseq', 'RPPA']:
         X_set_comp[m] = X_set[m][np.sum(Mask, axis=1) == 4]
         X_set_incomp[m] = X_set[m][np.sum(Mask, axis=1) != 4]
     Y_comp = Y[np.sum(Mask, axis=1) == 4]
